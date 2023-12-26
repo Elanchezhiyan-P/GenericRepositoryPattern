8feed0e3378a534756f75fb6592866254fe521e6
Elanchezhiyan P <elanche97@hotmail.com>
2023-12-26 12:48:14 +0530

    Added DataCollector library

---
+++ DataCollectorLibrary/Utils/JsonUtil.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace DataCollectorLibrary.Utils
+{
+    public static class JsonUtil
+    {
+        public static string Serialize<T>(T payload)
+        {
+            return JsonConvert.SerializeObject(payload, Newtonsoft.Json.Formatting.None,
+                new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+        }
+
+        public static T Deserialize<T>(string message)
+        {
+            return JsonConvert.DeserializeObject<T>(message);
+        }
+    }
+}
